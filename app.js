55314aac7fbaf8d3ff23785812bec56f837a08d7
Kolonel-K <72299237+Kolonel-K@users.noreply.github.com>
2020-10-23 23:40:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 // Book Class: Represents a Book
 class Book {
-  constructor(title, type, author, isbn) {
+  constructor(title, author, isbn) {
     this.title = title;
-    this.type = type;
     this.author = author;
     this.isbn = isbn;
   }
@@ -23,7 +22,6 @@ class UI {
 
     row.innerHTML = `
         <td>${book.title}</td>
-        <td>${book.type}</td>
         <td>${book.author}</td>
         <td>${book.isbn}</td>
         <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
@@ -52,7 +50,6 @@ class UI {
 
   static clearFields() {
     document.querySelector("#title").value = "";
-    document.querySelector("#type").value = "";
     document.querySelector("#author").value = "";
     document.querySelector("#isbn").value = "";
   }
@@ -100,16 +97,15 @@ document.querySelector("#book-form").addEventListener("submit", e => {
 
   // Get form values
   const title = document.querySelector("#title").value;
-  const type = document.querySelector("#type").value;
   const author = document.querySelector("#author").value;
   const isbn = document.querySelector("#isbn").value;
 
   // Validate
-  if (title === "" || type === "" || author === "" || isbn === "") {
+  if (title === "" || author === "" || isbn === "") {
     UI.showAlert("Please fill in all fields", "danger");
   } else {
     // Instatiate book
-    const book = new Book(title, type,  author, isbn);
+    const book = new Book(title, author, isbn);
 
     // Add Book to UI
     UI.addBookToList(book);
